2c6da773af31b4502dfaa8cac85c0aeba967f30c
msamgan <mohdsamgankhan@gmail.com>
2023-08-02 12:53:41 -0400

    package init

---
+++ cli/build.js
@@ -0,0 +1,83 @@
+const fs = require("fs")
+const path = require("path")
+const rootPath = path.join(__dirname, "../../../../")
+
+const builder = async (filePath, stubName) => {
+    if (fs.existsSync(filePath)) {
+        console.log(filePath + ` already exists.`)
+        return false
+    }
+
+    const stub = fs.readFileSync(path.join(__dirname, "../stubs/" + stubName), "utf8")
+
+    let dirs = filePath.split("/")
+    dirs.pop()
+
+    const dir = dirs.join("/")
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, {recursive: true})
+    }
+
+    return fs.writeFile(filePath, stub, (err) => {
+        if (err) {
+            console.log(err)
+
+            return false
+        }
+
+        return true
+    })
+}
+
+/**
+ * Creates a controller file for the given controller name.
+ * @param {string} name - The name of the controller.
+ * @returns None
+ */
+exports.buildController = async (name) => {
+    const filePath = path.join(rootPath, "controllers", `${name}.controller.js`)
+
+    if (await builder(filePath, "controller.stubs")) {
+        console.log(`Controller ${name}.controller.js created successfully`)
+    }
+}
+
+/**
+ * Creates a repository file for the given repository name.
+ * @param {string} name - The name of the repository.
+ * @returns None
+ */
+exports.buildRepository = async (name) => {
+    let repoDir = path.join(rootPath, "repositories")
+
+    if (!fs.existsSync(repoDir)) {
+        fs.mkdirSync(repoDir, {recursive: true})
+    }
+
+    const filePath = path.join(repoDir, `${name}.repository.js`)
+
+    if (await builder(filePath, "repository.stubs")) {
+        console.log(`Repository ${name}.repository.js created successfully`)
+    }
+}
+
+/**
+ *
+ * @param name
+ * @returns {Promise<void>}
+ */
+exports.buildValidator = async (name) => {
+    const filePath = path.join(rootPath, "middlewares/validators", `${name}.validations.js`)
+
+    if (await builder(filePath, "validator.stubs")) {
+        console.log(`Validator ${name}.validation.js created successfully`)
+    }
+}
+
+exports.buildMiddleware = async (name) => {
+    const filePath = path.join(rootPath, "middlewares", `${name}.middleware.js`)
+
+    if (await builder(filePath, "middleware.stubs")) {
+        console.log(`Middleware ${name}.middleware.js created successfully`)
+    }
+}
