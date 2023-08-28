398322aff17c55537f471a39f21e507da31b5b6b
Mohd Samgan Khan <mohdsamgankhan@gmail.com>
2023-08-28 16:09:36 -0400

    Routes command (#1)
    * added router command.
    
    * format.

---
+++ commands/build_route.js
@@ -0,0 +1,16 @@
+const { buildRoute } = require("../cli/build")
+
+exports.command = (program) => {
+    program
+        .command("build:route")
+        .description("Build Route, Usage: node x build:route <command-name>")
+        .argument("<routeName>", "Route Name")
+        .action(async (routeName) => {
+            console.log(`Building Route: ${routeName}`)
+            await buildRoute(routeName)
+            console.info(`Route ${routeName} built.`)
+            console.info(`Add the following to your index.js:`)
+            console.info(`    const ${routeName}Routes = require("./routes/${routeName}.route")`)
+            console.info(`    app.use("/${routeName}", ${routeName}Routes)`)
+        })
+}
