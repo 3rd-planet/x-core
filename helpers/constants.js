2d1320871e733e5a2f5b6bcb928687828f904e13
msamgan <mohdsamgankhan@gmail.com>
2023-09-12 10:48:59 -0400

    stable.

--- helpers/constants.js
+++ helpers/constants.js
@@ -2,6 +2,6 @@ const { rootPath } = require("../paths")
 let packageJson = require(rootPath + "../package.json")
 
 exports.CACHE_ENABLED = process.env.CACHE_ENABLED
-exports.CACHE_EXPIRE_TIME = process.env.CACHE_EXPIRE_TIME
 exports.CACHE_UPDATE_INTERVAL = process.env.CACHE_UPDATE_INTERVAL
+exports.CACHE_EXPIRE_TIME = process.env.CACHE_EXPIRE_TIME
 exports.CACHE_KEY_PREFIX = packageJson.name
